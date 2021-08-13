f68deb1ed55bfbf8c26f8087c46458f77bcee37a
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-13 11:41:48 +0530

    feat:Nlog

---
+++ NLogdemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NlogSession
+{
+    class Program
+    {
+        private static string result;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition on numbers Enter N1,N2 values : " + result);
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            var add = new Addnumber();
+            Console.WriteLine("Result is " + " " + add.Sum(a, b));
+
+        }
+    }
+}
